52bfa6a5359003736a2f1e87d8362bd7bf6455f6
kishikawa katsumi <katsumi@Mac.localdomain>
2024-06-20 06:45:32 +0900

    Use Translation API on macOS 15

---
+++ TextChatTranslator/TranslationContext.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+import Translation
+
+@Observable
+class TranslationContext {
+  var configuration = {
+    let userDefaults = UserDefaults.standard
+    let sourceLanguage = userDefaults.string(forKey: "sourceLanguage") ?? "en-Latn-US"
+    let targetLanguage = userDefaults.string(forKey: "targetLanguage") ?? "ja-Jpan-JP"
+    return TranslationSession.Configuration(
+      source: Locale.Language(identifier: sourceLanguage),
+      target: Locale.Language(identifier: targetLanguage)
+    )
+  }()
+
+  var session: TranslationSession?
+}
+
+extension EnvironmentValues {
+  @Entry var translationContext = TranslationContext()
+}
